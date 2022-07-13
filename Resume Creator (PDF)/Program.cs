3b195a547600548bbb7c43437a43695c0f493b5d
Ma. Angelica Sevilla <angelmaesevilla@gmail.com>
2022-07-13 17:42:06 +0800

    First commit - just saving this

---
+++ Resume Creator (PDF)/Program.cs
@@ -0,0 +1,17 @@
+namespace Resume_Creator__PDF_
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
